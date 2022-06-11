fb26f6a2894463f3693aa4f9f67b066b98cd1476
Christian O'Connor <coconnor98@comcast.net>
2022-06-11 16:53:15 -0400

    set auth test to register, logout, then login

--- TestRunProject.Tests/IntegrationTests/AuthTests.cs
+++ TestRunProject.Tests/IntegrationTests/AuthTests.cs
@@ -4,14 +4,12 @@ using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using Microsoft.Net.Http.Headers;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.Net.Http.Headers;
 using AngleSharp.Html.Dom;
 using Xunit;
 using TestRunProject.Data;
@@ -32,100 +30,56 @@ namespace TestRunProject.Tests
         }
 
         [Fact]
-        public async Task Post_FirsttoRegisterClaimsAdmin()
+        public async Task DLoginTest()
         {
             var client = _factory.CreateClient(
                 new WebApplicationFactoryClientOptions
                 {
                     AllowAutoRedirect = true
                 });
-            var initResponse = await client.GetAsync("/Identity/Account/Register");
-            var antiForgeryValues = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(initResponse);
-
-            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register");
-            postRequest.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
+            var newAntiForg = await client.GetAsync("/Identity/Account/Register");
+            var antiForgeryValues = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(newAntiForg);
+            var postRequest2 = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register");
+            postRequest2.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
             var formModel = new Dictionary<string, string>
             {
                 { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue },
-                { "Email", "test@example.com" },
-                { "Password", "pas3w0!rRd" },
-                { "ConfirmPassword", "pas3w0!rRd" },
+                { "Email", "test2@example.com" },
+                { "Password", "pas3w02!rRd" },
+                { "ConfirmPassword", "pas3w02!rRd" },
             };
-            postRequest.Content = new FormUrlEncodedContent(formModel);
-            var response = await client.SendAsync(postRequest);
-            response.EnsureSuccessStatusCode();
 
-            var postRequest2 = new HttpRequestMessage(HttpMethod.Post, "/Home/SetAdminToUser");
-            postRequest2.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
-            var formModel2 = new Dictionary<string, string>
-            {
-                { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue },
-                { "check1", "True" },
-                { "check2", "True" },
-                { "check3", "True" },
-            };
-            postRequest2.Content = new FormUrlEncodedContent(formModel2);
+            postRequest2.Content = new FormUrlEncodedContent(formModel);
             var response2 = await client.SendAsync(postRequest2);
             response2.EnsureSuccessStatusCode();
 
-            Assert.Equal("http://localhost/Home/ClaimAdmin", response.RequestMessage.RequestUri.ToString());
-            Assert.Equal("http://localhost/Admin/MasterWalletList", response2.RequestMessage.RequestUri.ToString());
-
-            IdentityUserRole<string>? userRl;
-            IdentityUser? user;
-            using (var scope = _factory.Services.CreateScope())
-            {
-                var scopedServices = scope.ServiceProvider;
-                var db = scopedServices.GetRequiredService<ApplicationDbContext>();
-                user = db.Users.FirstOrDefault();
-                userRl = db.UserRoles.FirstOrDefault();
-            }
-
-            Assert.NotNull(user);
-            Assert.True(userRl?.UserId == user?.Id);
-        }
-
-        [Fact]
-        public async Task CreateMasterWalletTest()
-        {
-            var client = _factory.CreateClient(
-                new WebApplicationFactoryClientOptions
-                {
-                    AllowAutoRedirect = true
-                });
-
-            var initResponse = await client.GetAsync("/Admin/MasterWalletList");
-            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Admin/CreateMasterWallet");
-            var antiForgeryValues = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(initResponse);
-            postRequest.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
-            var formModel = new Dictionary<string, string>
+            var profResponse = await client.GetAsync("/Profile");
+            //var antiForgeryValues2 = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(initResponse);
+            var postRequestLogout = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Logout");
+            postRequestLogout.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
+            var formModel2 = new Dictionary<string, string>
             {
                 { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue },
-                { "label", "testWallet1t" },
-                { "isTestNet", "True" }
             };
-            postRequest.Content = new FormUrlEncodedContent(formModel);
-            var response = await client.SendAsync(postRequest);
-            response.EnsureSuccessStatusCode();
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            
-            IdentityUserRole<string>? userRl;
-            MasterWallet? wallet1;
-            IdentityUser user;
-            using (var scope = _factory.Services.CreateScope())
-            {
-                var scopedServices = scope.ServiceProvider;
-                var db = scopedServices.GetRequiredService<ApplicationDbContext>();
-                wallet1 = db.MasterWallets.FirstOrDefault();
-                userRl = db.UserRoles.FirstOrDefault();
-                user = db.Users.FirstOrDefault();
-            }
+            postRequestLogout.Content = new FormUrlEncodedContent(formModel2);
 
-            Assert.Equal(user.Email, "test@example.com");
-            //Assert.Equal(userRl?.UserId, user?.Id);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.NotNull(wallet1);
-            Assert.Equal(wallet1?.Address.Substring(0, 3), "tb1");
+            var logoutAnswer = await client.SendAsync(postRequestLogout);
+            Console.WriteLine(logoutAnswer.StatusCode);
+            logoutAnswer.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, logoutAnswer.StatusCode);
+
+            var postRequest3 = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Login");
+            postRequest3.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
+            var formModel3 = new Dictionary<string, string>
+            {
+                { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue },
+                { "Input.Email", "test2@example.com" },
+                { "Input.Password", "pas3w02!rRd" }
+            };
+            postRequest3.Content = new FormUrlEncodedContent(formModel3);
+            var reLoginResponse = await client.SendAsync(postRequest3);
+            reLoginResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, reLoginResponse.StatusCode);
         }
 
         public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
