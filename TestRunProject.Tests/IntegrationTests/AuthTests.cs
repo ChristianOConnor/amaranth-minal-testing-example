8acde318877ac02ef54946fb35869e135a913f65
Christian O'Connor <coconnor98@comcast.net>
2022-06-19 23:22:36 -0400

    initial

--- TestRunProject.Tests/IntegrationTests/AuthTests.cs
+++ TestRunProject.Tests/IntegrationTests/AuthTests.cs
@@ -1,92 +1,73 @@
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http.Headers;
-using System.Security.Claims;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using AngleSharp.Html.Dom;
-using Xunit;
-using TestRunProject.Data;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.Net.Http.Headers;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace TestRunProject.Tests
 {
-    public class AdminControllerTests :
-        IClassFixture<CustomWebApplicationFactory<TestRunProject.Startup>>
+    public class AuthTests
     {
-        private readonly CustomWebApplicationFactory<TestRunProject.Startup>
-            _factory;
-
-        public AdminControllerTests(
-            CustomWebApplicationFactory<TestRunProject.Startup> factory)
-        {
-            _factory = factory;
-        }
-
         [Fact]
         public async Task DLoginTest()
         {
-            var client = _factory.CreateClient(
-                new WebApplicationFactoryClientOptions
+            {
+                var application = new WebApplicationFactory<Program>()
+                    .WithWebHostBuilder(builder =>
+                    {
+                        // ... Configure test services
+                    });
+
+                var client = application.CreateClient();
+
+                // -- REGISTER --
+
+                var registerResponse = await client.GetAsync("/Identity/Account/Register");
+                registerResponse.EnsureSuccessStatusCode();
+                string registerResponseContent = await registerResponse.Content.ReadAsStringAsync();
+
+                var requestVerificationToken = AntiForgeryTokenExtractor.ExtractAntiForgeryToken(registerResponseContent);
+                
+                var formModel = new Dictionary<string, string>
                 {
-                    AllowAutoRedirect = true
-                });
-            var newAntiForg = await client.GetAsync("/Identity/Account/Register");
-            var antiForgeryValues = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(newAntiForg);
-            var postRequest2 = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register");
-            postRequest2.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
-            var formModel = new Dictionary<string, string>
-            {
-                { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue },
-                { "Input.Email", "test2@example.com" },                
-                { "Input.Password", "pas3w02!rRd" },
-                { "Input.ConfirmPassword", "pas3w02!rRd" }
-            };
+                    { "Input.Email", "test2@example.com" },
+                    { "Input.Password", "pas3w02!rRd" },
+                    { "Input.ConfirmPassword", "pas3w02!rRd" },
+                    { "__RequestVerificationToken", requestVerificationToken },
+                };
 
-            postRequest2.Content = new FormUrlEncodedContent(formModel);
-            var response = await client.SendAsync(postRequest2);
-            response.EnsureSuccessStatusCode();
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                var postRequest2 = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register");
+                postRequest2.Content = new FormUrlEncodedContent(formModel);
+                var registerResponse2 = await client.SendAsync(postRequest2);
+                registerResponse2.EnsureSuccessStatusCode();
 
-            IdentityUser userObj;
+                // -- LOGIN --
 
-            using (var scope = _factory.Services.CreateScope())
-            {
-                var scopedServices = scope.ServiceProvider;
-                var db = scopedServices.GetRequiredService<ApplicationDbContext>();
-                var firstUser = db.Users.FirstOrDefault();
-                userObj = firstUser;
-                Assert.Equal(1, db.Users.Count());
-                Assert.Equal("test2@example.com", firstUser?.Email);
+                var loginResponse = await client.GetAsync("/Identity/Account/Login");
+                loginResponse.EnsureSuccessStatusCode();
+                string loginResponseContent = await registerResponse.Content.ReadAsStringAsync();
 
-                //var rootCall = await client.GetAsync("/");
+                requestVerificationToken = AntiForgeryTokenExtractor.ExtractAntiForgeryToken(loginResponseContent);
 
-                var signInMngInst = scopedServices.GetRequiredService<SignInManager<IdentityUser>>();
-                await signInMngInst.Context.GetTokenAsync("/");
-                await signInMngInst.SignOutAsync();
+                formModel = new Dictionary<string, string>
+                {
+                    { "Input.Email", "test2@example.com" },
+                    { "Input.Password", "pas3w02!rRd" },
+                    { "__RequestVerificationToken", requestVerificationToken },
+                };
+
+                var loginRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Login");
+                loginRequest.Content = new FormUrlEncodedContent(formModel);
+                var loginResponse2 = await client.SendAsync(loginRequest);
+                loginResponse2.EnsureSuccessStatusCode();
+
+                // -- LOGOUT --
+
+                var logoutRequest = new StringContent("");
+                logoutRequest.Headers.Add("RequestVerificationToken", requestVerificationToken);
+
+                var logoutResponse = await client.PostAsync("/Identity/Account/Logout", logoutRequest);
+
+                logoutResponse.EnsureSuccessStatusCode();
             }
-
-            /* var signInMng = _factory.Services.GetService<SignInManager<IdentityUser>>();
-            signInMng.SignOutAsync(); */
-
-            var postRequest3 = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Login");
-            postRequest3.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
-            var formModel3 = new Dictionary<string, string>
-            {
-                { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue },
-                { "Input.Email", "test2@example.com" },
-                { "Input.Password", "pas3w02!rRd" }
-            };
-            postRequest3.Content = new FormUrlEncodedContent(formModel3);
-            var reLoginResponse = await client.SendAsync(postRequest3);
-            reLoginResponse.EnsureSuccessStatusCode();
-            Assert.Equal(HttpStatusCode.OK, reLoginResponse.StatusCode);
         }
     }
 }
